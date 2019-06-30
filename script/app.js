3e0dcdcaa8e6f4114de6eeb86ef9425695499fba
N-bred <thehardyboiz1@gmail.com>
2019-06-30 00:34:15 -0500

    Finish loader and header styles

--- script/app.js
+++ script/app.js
@@ -1,2 +1,11 @@
 // Repos api https://api.github.com/users/N-bred/repos
 // User api https://api.github.com/users/N-bred
+
+window.addEventListener('load', function() {
+  const loader = document.querySelector('.loader');
+
+  //   Change time to 2 seconds
+  setTimeout(() => {
+    loader.classList.add('loader-done');
+  }, 100);
+});
